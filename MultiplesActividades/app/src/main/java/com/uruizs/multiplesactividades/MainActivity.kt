d73fbfe09a26d3d0cb718fa751dd763e02b6aa11
super <supertepo@outlook.com>
2026-09-22 19:07:24 -0600

    Creando el entregable

---
+++ MultiplesActividades/app/src/main/java/com/uruizs/multiplesactividades/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.uruizs.multiplesactividades
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.uruizs.multiplesactividades.ui.theme.MultiplesActividadesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MainUI()
+        }
+    }
+}
+@Composable
+fun MainUI(){
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val contexto = LocalContext.current
+
+    Column(modifier = Modifier.fillMaxSize().padding(top = 40.dp)) {
+        Row(Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically) {
+            TextField(value=email, onValueChange = {email = it}, label = {Text("Usuario")})
+        }
+        Row(Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,) {
+            TextField(value = password,
+                onValueChange = {password = it}, label = {Text("contraseña")},
+                visualTransformation = PasswordVisualTransformation())
+        }
+        Row(Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            ) {
+            Button(onClick = {
+                val emailV="admin"
+                val passV="admin"
+
+                if (emailV == email && passV == password){
+                    val intento= Intent(contexto, Activity2::class.java)
+                    contexto.startActivity(intento)
+                }else {
+                    Toast.makeText(contexto, "Usuario no valido", Toast.LENGTH_SHORT).show()
+
+                }
+            }) { Text("ingresar")}
+        }
+    }
+}
